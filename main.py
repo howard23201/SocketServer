2b7ee27f171fe3412b5f49eb2ebf8f25b2b3deeb
howard23201 <howard23201@gmail.com>
2023-03-30 16:59:22 +0800

    20230330
    1.增加確認ip功能
    2.增加開啟server時，選擇ip功能

--- main.py
+++ main.py
@@ -8,28 +8,38 @@ def Main():
     Net = Networks()
     ips = Net.GetIP()
     server = None
+    ServerIP = ""
     while(True):
         try:
             print("\n-------------------\n"+
                 "1: start server\n"+
+                "2: send msg\n"+
                 "3: stop server\n"+
-                "4: fsffslkgj;lgh\n"+
+                "4: Check ip address\n"+
                 "-------------------\n")
-            str = input('please enter a number:\n')
-            if(str == '1'):
-                print('local ip: {0}:{1},waiting for a client connection...'.format(ips[0],10000))
-                server = Server(ips[0],10000)
+            Cmd = input('please enter a number:\n')
+            if(Cmd == '1'):
+                strIP = ""
+                i = 0
+                for i in range(len(ips)):
+                    cnt = str(i + 1)
+                    strIP += "{0} => {1}\n".format(cnt,ips[i])
+                num = int(input("Please choose an IP address to start.\n{0}\n=>".format(strIP)))
+                
+                print('local ip: {0}:{1},waiting for a client connection...'.format(ips[num - 1],10000))
+                server = Server(ips[num - 1],10000)
+                ServerIP = "{0}:{1}".format(ips[num - 1],10000)
                 Sthread = Thread(target = server.Start)
                 Sthread.start()
-            # elif(str == '2'):
-            #     msg = input('please enter cmd:')
-            #     server.SendCommand(msg)
-            #     print("server sent: {0}\n".format(msg))
-            elif(str == '3'):
+            elif(Cmd == '2'):
+                msg = input('please enter cmd:')
+                server.SendCommand(msg)
+                print("server sent: {0}\n".format(msg))
+            elif(Cmd == '3'):
                 server.Stop()  
                 print("server closed\n")
-            elif(str == '4'):       
-                print("fsffslkgj;lgh\n")
+            elif(Cmd == '4'):       
+                print('Server IP address: {0}\n'.format(ServerIP))
             else:
                 print("undefine code\n")
         except Exception as ex:
